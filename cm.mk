d0dba4b8c3329069837a99d8b41a5d9fa38be27d
Andreas Blaesius <skate4life@gmx.de>
2015-07-28 23:13:33 +0200

    P3110: set ro.carrier to wifi-only
    Respect:
      From: Andreas Blaesius <skate4life@gmx.de>
      Date: Mon, 3 Aug 2015 21:51:19 +0200
      Subject: [PATCH] P3110: ro.carrier needs to be defined earlier
    
      ... else it get overwriten by generic_no_telephony.mk which
      get inherit from full_base.mk
    
      ( Line 58+59 https://android.googlesource.com/platform/build/+/android-5.1.1_r8/target/product/generic_no_telephony.mk )
    
      Change-Id: Id974e3509b8fb09a6af17e9807b08289bf80c982
    
    Change-Id: I4ecc05c919b5ce4243cfc4dbe0a5bbf1fb3ace75

--- cm.mk
+++ cm.mk
@@ -21,6 +21,9 @@ PRODUCT_RELEASE_NAME := p3110
 TARGET_SCREEN_HEIGHT := 1024
 TARGET_SCREEN_WIDTH := 600
 
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.carrier=wifi-only
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
