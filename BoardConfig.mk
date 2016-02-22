ff54a320aa635e7dc9e43d3fa8526667383d310e
Ziyan <jaraidaniel@gmail.com>
2016-02-22 21:19:27 +0100

    Move TARGET_KERNEL_CONFIG to espresso-common (3/5)
    Change-Id: I79f5d37cc0985feb20203e4d8762300449c9f551

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,6 @@
 
 TARGET_BOARD_INFO_FILE := device/samsung/p3110/board-info.txt
 
-# Inline kernel building
-TARGET_KERNEL_CONFIG := cyanogenmod_p3110_defconfig
-
 # assert
 TARGET_OTA_ASSERT_DEVICE := espresso-common,p3110,GT-P3110,p3113,GT-P3113,espressowifi,espressowifixx
 
